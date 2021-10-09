d1b75c5d0ffffc8878ab29e40d67c9ed6ad5ced2
Yan Medardo Minchan <yanmi@DESKTOP-B7A077C>
2021-10-08 20:38:01 -0500

    Commit 3

---
+++ CalidadT2/CalidadT2/Repository/UsuarioRepository.cs
@@ -0,0 +1,39 @@
+﻿using CalidadT2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace CalidadT2.Repository
+{
+
+    public interface IUsuarioRepository
+    {
+        public Usuario FindUser(string username, string password);
+        public Usuario UserLogued(Claim claim);
+    }
+
+    public class UsuarioRepository : IUsuarioRepository
+    {
+
+        private AppBibliotecaContext _context;
+
+        public UsuarioRepository(AppBibliotecaContext context)
+        {
+            _context = context;
+        }
+
+        public Usuario FindUser(string username, string password)
+        {
+            var user = _context.Usuarios.FirstOrDefault(o => o.Username == username && o.Password == password);
+            return user;
+        }
+
+        public Usuario UserLogued(Claim claim)
+        {
+            var user = _context.Usuarios.FirstOrDefault(o => o.Username == claim.Value);
+            return user;
+        }
+    }
+}
---
+++ CalidadT2/CalidadT2/Service/CookieAuthService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CalidadT2.Service
+{
+    public class CookieAuthService
+    {
+    }
+}
